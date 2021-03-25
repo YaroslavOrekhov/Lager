e81dbf7a0334dab8cf226981ccdb4d17e7c827e8
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-03-25 03:20:58 +0300

    Added comments

--- api/api_back.php
+++ api/api_back.php
@@ -2,20 +2,63 @@
 
 require_once 'connection.php'; //подключаем скрипт подключения БД
 
-//подключаемся к серверу
-$link = mysql_connect($host, $user, $password, $database)
-    or die('Ошибка ' .mysql_error($link));
+echo 'host: '.$host.'<br>';
+echo 'user: '.$user.'<br>';
+echo 'password: '.$password.'<br>';
+echo 'database: '.$database.'<br>';
 
-//операции с БД
-$query ="SELECT * FROM child_group";
-$result = mysql_query($link, $query) or die ('Ошибка ' .mysql_error($link));
-if($result)
-{
-    echo "Выполнение запроса прошло успешно";
-    echo $result;
+//подключаемся к серверу
+$link = mysqli_connect($host, $user, $password, $database)
+    or die('Ошибка ' .mysqli_error($link));
+
+// Check connection
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+  }
+
+// echo gettype($link);
+// var_dump($link);  
+$query = "SELECT * FROM child_group";
+$result = mysqli_query($link, $query);
+// var_dump( $result);
+// echo $result;
+if ($result){
+    $rows = mysqli_num_rows($result);
+    echo $rows;
+
+    echo "<table><tr><th>Id</th><th>Num</th><th>Name</th></tr>";
+    for ($i = 0 ; $i < $rows ; ++$i)
+    {
+        $row = mysqli_fetch_row($result);
+        echo "<tr>";
+            for ($j = 0 ; $j < 3 ; ++$j) echo "<td>$row[$j]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+     
+    // очищаем результат
+    mysqli_free_result($result);
 }
 
+function get_data(){
+
+    $query = "SELECT * FROM child_group";
+    
+    if($result = mysqli_query($link, $query) === true){
+        echo 'Succec';
+    } else {
+        echo 'fail';
+    }
+    //  or die ('Ошибка ' .mysql_error($link));
+    return $result;
+};
+
+// $a = get_data();
+//  echo $a;
+
+    
+
 //закрытие подключения к БД
-mysql_close($link);
+mysqli_close($link);
 
 ?>
--- pages/main.php
+++ pages/main.php
@@ -1,7 +1,12 @@
 <?php
-
+    include_once 'api/api_back.php';
 ?>
 
 <main>
     <p>main content</p>
+    <?php
+        $data = get_data();
+        
+        echo $data;
+    ?>
 </main>
