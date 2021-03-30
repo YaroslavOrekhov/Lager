71a263954f693a50ef77869d5e43ab67fff47c63
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-03-31 02:23:18 +0300

    create method for send data

--- api/api_back.php
+++ api/api_back.php
@@ -2,11 +2,6 @@
 
 require_once 'connection.php'; //подключаем скрипт подключения БД
 
-echo 'host: '.$host.'<br>';
-echo 'user: '.$user.'<br>';
-echo 'password: '.$password.'<br>';
-echo 'database: '.$database.'<br>';
-
 //подключаемся к серверу
 $link = mysqli_connect($host, $user, $password, $database)
     or die('Ошибка ' .mysqli_error($link));
@@ -14,49 +9,38 @@ $link = mysqli_connect($host, $user, $password, $database)
 // Check connection
 if ($link->connect_error) {
     die("Connection failed: " . $link->connect_error);
-  }
+}
 
-// echo gettype($link);
-// var_dump($link);  
+
 $query = "SELECT * FROM child_group";
 $result = mysqli_query($link, $query);
-// var_dump( $result);
-// echo $result;
-if ($result){
-    $rows = mysqli_num_rows($result);
-    echo $rows;
 
-    echo "<table><tr><th>Id</th><th>Num</th><th>Name</th></tr>";
+$rows = mysqli_num_rows($result);
+
+if ($result){
+    $output = '';
+    $rows = mysqli_num_rows($result);
+    $row;
+
+    $number=[];
+    $name=[];
     for ($i = 0 ; $i < $rows ; ++$i)
     {
         $row = mysqli_fetch_row($result);
-        echo "<tr>";
-            for ($j = 0 ; $j < 3 ; ++$j) echo "<td>$row[$j]</td>";
-        echo "</tr>";
+        $number[$i].=$row[1];
+        $name[$i].=$row[2];
     }
-    echo "</table>";
-     
-    // очищаем результат
-    mysqli_free_result($result);
+
+    $out = json_encode(array(
+        count=>$rows,
+        number=>$number,
+        name=>$name
+    ));
+    echo $out;    
 }
 
-function get_data(){
-
-    $query = "SELECT * FROM child_group";
-    
-    if($result = mysqli_query($link, $query) === true){
-        echo 'Succec';
-    } else {
-        echo 'fail';
-    }
-    //  or die ('Ошибка ' .mysql_error($link));
-    return $result;
-};
-
-// $a = get_data();
-//  echo $a;
-
-    
+// очищаем результат
+mysqli_free_result($result);
 
 //закрытие подключения к БД
 mysqli_close($link);
