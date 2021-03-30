73dd84052a3f80c3fcfd5543631fc7ad1da9c731
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-03-31 02:21:18 +0300

    added ajax function for get data from table

--- pages/main.php
+++ pages/main.php
@@ -1,12 +1,30 @@
-<?php
-    include_once 'api/api_back.php';
-?>
-
 <main>
     <p>main content</p>
-    <?php
-        $data = get_data();
-        
-        echo $data;
-    ?>
+        <div id='table'></div>
+    <style>
+        .red{
+            background-color: red;
+        }
+    </style>
 </main>
+<script type='text/javascript'>
+var data
+        $.ajax({
+            url:'api/api_back.php',
+            type: 'GET',
+            dataType: "json",
+            // data: {
+            //     method: 'get_data'
+            // },
+            success: function(result){
+                // result = parseJSON(result);
+                $('#table').html(result);
+                document.getElementById('l').setAttribute("class", "red");
+                // data = result;
+            },
+            error: function(result){
+                console.log(result);
+                console.log('error');
+            }
+        });
+    </script>
