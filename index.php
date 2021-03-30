7ef3d9ba2dd3578e528bc0b86834bbf21b19f362
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-03-31 02:21:57 +0300

    initial jQuery library

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <script type='text/javascript' src='js/jquery-3.6.0.min.js'></script>
     <title>Document</title>
 </head>
 <body>
