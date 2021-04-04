8c961384f2cdcdc87a6dfe3f55ef33c9bc61141a
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-04-04 20:13:41 +0300

    added tags for output from back

--- pages/main.php
+++ pages/main.php
@@ -1,9 +1,32 @@
 <main>
     <p>main content</p>
-        <div id='table'></div>
+        <!-- <div id='table'>
+            <div class='table_row'>
+                <div>id</div>
+                <div>Num</div>
+                <div>Name</div>
+            </div>
+        </div> -->
+
+        <table id='table'>  
+            <tr>
+                <td>id</td>
+                <td>Num</td>
+                <td>Name</td>
+            </tr>
+        </table>
     <style>
         .red{
-            background-color: red;
+            background-color: white;
+        }
+
+        table, td, tr{
+            border: solid 1px grey;
+            border-spacing: 0px;
+        }
+
+        td{
+            padding: .1em 1em;
         }
     </style>
 </main>
@@ -18,9 +41,40 @@ var data
             // },
             success: function(result){
                 // result = parseJSON(result);
-                $('#table').html(result);
-                document.getElementById('l').setAttribute("class", "red");
-                // data = result;
+                // $('#table').html(result);
+                data = result;
+                console.log(result);
+
+                let table = document.getElementById('table');
+                table.setAttribute("class", "red table");
+                // table.setAttribute('class', 'table')
+
+                let rows;         
+                                
+                let count = result.count;
+                let numbers = result.number;
+                let names = result.name;
+
+                
+
+                for(i = 0; i < count; i++){
+                    // console.log(i);
+                    // let row = document.createElement('div');
+                    let row = document.createElement('tr');
+
+
+                    row.setAttribute('class', 'table_row');
+                    row.setAttribute('id', 'row'+i);
+                    // let el =  '<div>'+i+'</div><div>'+numbers[i]+'</div><div>'+names[i]+'</div>';
+                    // row.html = el;
+                    row.innerHTML = '<td>'+i+'</td><td>'+numbers[i]+'</td><td>'+names[i]+'</td>';
+
+                    
+                    $("#table").append(row);
+                    // console.log(row)
+
+
+                }
             },
             error: function(result){
                 console.log(result);
