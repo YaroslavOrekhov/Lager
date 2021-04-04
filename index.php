4084c5d079608ec1398d0ecaff3739fa79bcdf0c
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-04-04 20:10:39 +0300

    added hiden menu

--- index.php
+++ index.php
@@ -10,13 +10,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
     <script type='text/javascript' src='js/jquery-3.6.0.min.js'></script>
+    <script type='text/javascript' src='js/ui.js'></script>
     <title>Document</title>
 </head>
 <body>
+    
     <?php
+    include_once 'pages/navigation.php';
+    echo '<div class="site">';
     include_once 'pages/header.php'; // Вставка шапки
     include_once 'pages/main.php';  // Вставка основного блока
     include_once 'pages/footer.php'; // Вставка футера
+    echo '</div>';
     ?>
 </body>
 </html>
