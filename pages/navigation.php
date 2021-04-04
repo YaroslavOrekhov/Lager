02baa37b8b651af7b42c1fe1f8cddffc4d4fbcf9
Yaroslav orekhov <yaroslav.orehoff@gmail.com>
2021-04-04 20:13:57 +0300

    initial hiden menu

---
+++ pages/navigation.php
@@ -0,0 +1,7 @@
+<ul class="navigation">
+    <li class="nav-item"><a href="#">Главная</a></li>
+    <li class="nav-item"><a href="#">Портфолио</a></li>
+    <li class="nav-item"><a href="#">О нас</a></li>
+    <li class="nav-item"><a href="#">Блог</a></li>
+    <li class="nav-item"><a href="#">Контакты</a></li>
+</ul>
